256dfc9ba31edcfadf90395fe10dc76dfeebc65b
dmarjanovic94 <dmarjanovic94@gmail.com>
2015-11-03 17:33:37 +0100

    Modified main.py.

---
+++ tic-tac-toe/main.py
@@ -0,0 +1,25 @@
+"""
+    Problem Task:
+        Play tic-tac-toe against humans.
+
+    Source of Experinece:
+        Play games against itself.
+
+    Measure of Performance:
+        Percent of wins.
+    ------------------------------------
+    Target Function:
+        Linear function.
+
+    Target Function Representatnion:
+        V = w0 + w1*x1 + ... + wn*xn
+        where wi (i = 1..n) represents weights.
+
+    Learning mechanism:
+        LMS (Least mean squares), gradient descent.
+"""
+
+def main():
+    pass
+
+main()
