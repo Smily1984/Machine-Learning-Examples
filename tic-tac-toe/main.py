df9a2b98c1cc60647185b1acf7358ff7e5be040e
dmarjanovic94 <dmarjanovic94@gmail.com>
2015-11-09 15:38:00 +0100

    Added some functions. Defined features.

--- tic-tac-toe/main.py
+++ tic-tac-toe/main.py
@@ -19,7 +19,50 @@
         LMS (Least mean squares), gradient descent.
 """
 
-def main():
+# Return list of free board positions
+def get_possible_positions(board):
+    possible_positions = []
+    for i in xrange(len(board)):
+        if '-' == board[i]:
+            possible_positions.append(i)
+
+    return possible_positions
+
+
+def gradient_descent():
+    # alpha = 0.1
+    # for each training example
+    #     V(b) = X * weights
+    #     wi = wi + alpha * (Vtrain(b) - V(b)) * xi
     pass
+
+def number_of_ox(board):
+    return board.count('O'), board.count('X')
+
+
+
+def number_of_two_consecutive_ox(board):
+    pass
+
+
+def number_of_three_consecutive_ox(board):
+    pass
+
+def main():
+
+    print " Playing against itself... Please wait... "
+
+    """
+        Features:
+        - x_0   1 (bias)
+        - x_1   Number of O's
+        - x_2   Number of X's
+        - x_3   Number of 2 consecutive O's
+        - x_4   Number of 2 consecutive X's
+        - x_5   Number of 3 consecutive O's (O won)
+        - x_6   Number of 3 consecutive X's (X won)
+    """
+
+    print number_of_ox(['X', 'O', '-', '-', 'X', 'X', '-', '-', 'O'])
 
 main()
